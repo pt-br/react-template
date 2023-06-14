4d05535f48d0943898bb8a9ed30b86ec834e1f09
Lucas P. Tavares <ltavares@yieldstreet.com>
2023-06-14 11:05:07 -0300

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
+  tabWidth: 2,
+  printWidth: 100
+}
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <TestDiv>AAAAAAA TEST DIV</TestDiv>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+const TestDiv = styled.div``;
+
+export default App;
